d6e3539c4edb446d13bdea477e100bdfb1a1a5fb
maso <mostafa.barmshory@dpq.co.ir>
2017-10-11 17:13:47 +0330

    Bug fixed: check dist of the project.

--- Gruntfile.js
+++ Gruntfile.js
@@ -320,27 +320,27 @@ module.exports = function(grunt) {
 		// minification. These next options are pre-configured if you do
 		// not wish
 		// to use the Usemin blocks.
-		// cssmin: {
-		// dist: {
-		// files: {
-		// '<%= yeoman.dist %>/styles/main.css': [
-		// '.tmp/styles/{,*/}*.css'
-		// ]
-		// }
-		// }
-		// },
-		// uglify: {
-		// dist: {
-		// files: {
-		// '<%= yeoman.dist %>/scripts/scripts.js': [
-		// '<%= yeoman.dist %>/scripts/scripts.js'
-		// ]
-		// }
-		// }
-		// },
-		// concat: {
-		// dist: {}
-		// },
+//		cssmin: {
+//			dist: {
+//				files: {
+//					'<%= yeoman.dist %>/styles/main.css': [
+//						'.tmp/styles/{,*/}*.css'
+//						]
+//				}
+//			}
+//		},
+//		uglify: {
+//			dist: {
+//				files: {
+//					'<%= yeoman.dist %>/scripts/scripts.js': [
+//						'<%= yeoman.dist %>/scripts/scripts.js'
+//						]
+//				}
+//			}
+//		},
+//		concat: {
+//			dist: {}
+//		},
 
 		imagemin : {
 			dist : {
@@ -412,7 +412,7 @@ module.exports = function(grunt) {
 		cdnify : {
 			dist : {
 				html : [ '<%= yeoman.dist %>/*.html' ],
-				bower: 'bower.json'
+//				bower: 'bower.json'
 			}
 		},
 
@@ -573,6 +573,8 @@ module.exports = function(grunt) {
 		injector: {
 			options: {
 				// Task-specific options go here.
+				relative: true,
+				addRootSlash: false
 			},
 			project_files: {
 				files: {
