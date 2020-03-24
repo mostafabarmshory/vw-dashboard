d02ccf769b5fe3596da4ac3c4bff83b6a66894f6
Mostafa Barmshory <mostafa.barmshory@gmail.com>
2020-03-24 15:37:44 +0430

    bug fixed: update config condition of tenants based on new model of application store.

--- app/scripts/tenant/configs/states.js
+++ app/scripts/tenant/configs/states.js
@@ -20,125 +20,125 @@
  * SOFTWARE.
  */
 
-angular.module('ngMaterialDashboardTenant').config(function ($routeProvider) {
-    $routeProvider
-        .when('/tenant/tenants', {
-            templateUrl: 'views/amd-tenant-tenants.html',
-            controller: 'AmdTenantTenantsController',
-            controllerAs: 'ctrl',
-            groups: ['tenant'],
-            navigate: true,
+angular.module('ngMaterialDashboardTenant').config(function($routeProvider) {
+	$routeProvider
+		.when('/tenant/tenants', {
+			templateUrl: 'views/amd-tenant-tenants.html',
+			controller: 'AmdTenantTenantsController',
+			controllerAs: 'ctrl',
+			groups: ['tenant'],
+			navigate: true,
             /*
              * @ngInject
              */
-            protect: function ($rootScope) {
-                return !$rootScope.account.permissions.tenant_owner;
-            },
-            loginRequired: true,
-            name: 'Tenants',
-            icon: 'business'
-        })
-        .when('/tenant/tenants/:tenantId', {
-            templateUrl: 'views/amd-tenant-tenant.html',
-		protect: true,
-            controller: 'AmdTenantTenantController',
-            controllerAs: 'ctrl'
-        })
-        .when('/tenant/tickets', {
-            templateUrl: 'views/amd-tenant/ticket-list.html',
-            controller: 'AmdTenantTicketsController',
-            groups: ['tenant'],
-            navigate: true,
-            name: 'Tickets',
-            icon: 'question_answer',
+			protect: function($rootScope) {
+				return !$rootScope.__account.permissions.tenant_owner;
+			},
+			loginRequired: true,
+			name: 'Tenants',
+			icon: 'business'
+		})
+		.when('/tenant/tenants/:tenantId', {
+			templateUrl: 'views/amd-tenant-tenant.html',
+			protect: true,
+			controller: 'AmdTenantTenantController',
+			controllerAs: 'ctrl'
+		})
+		.when('/tenant/tickets', {
+			templateUrl: 'views/amd-tenant/ticket-list.html',
+			controller: 'AmdTenantTicketsController',
+			groups: ['tenant'],
+			navigate: true,
+			name: 'Tickets',
+			icon: 'question_answer',
             /*
              * @ngInject
              */
-            protect: function ($rootScope) {
-                return !$rootScope.app.user.tenant_owner;
-            },
-        }) //
-        .when('/tenant/tickets/:ticketId', {
-            templateUrl: 'views/amd-tenant/ticket.html',
-		protect: true,
-            controller: 'AmdTenantTicketController',
+			protect: function($rootScope) {
+				return !$rootScope.__account.permissions.tenant_owner;
+			},
+		}) //
+		.when('/tenant/tickets/:ticketId', {
+			templateUrl: 'views/amd-tenant/ticket.html',
+			protect: true,
+			controller: 'AmdTenantTicketController',
             /*
              * @ngInject
              */
-            protect: function ($rootScope) {
-                return !$rootScope.app.user.tenant_owner;
-            },
-        })
-        .when('/tenant/invoices', {
-            templateUrl: 'views/amd-tenant/invoice-list.html',
-            controller: 'AmdTenantInvoicesController',
-            groups: ['tenant'],
-            navigate: true,
-            name: 'Invoices',
-            icon: 'attach_money',
-            helpId: 'invoice',
+			protect: function($rootScope) {
+				return !$rootScope.__account.permissions.tenant_owner;
+			},
+		})
+		.when('/tenant/invoices', {
+			templateUrl: 'views/amd-tenant/invoice-list.html',
+			controller: 'AmdTenantInvoicesController',
+			groups: ['tenant'],
+			navigate: true,
+			name: 'Invoices',
+			icon: 'attach_money',
+			helpId: 'invoice',
             /*
              * @ngInject
              */
-            protect: function ($rootScope) {
-                return !$rootScope.__account.permissions.tenant_owner;
-            },
-        }) //
-        .when('/tenant/invoices/new', {
-            templateUrl: 'views/amd-tenant/invoice-new.html',
-            controller: 'AmdTenantInvoiceController',
+			protect: function($rootScope) {
+				return !$rootScope.__account.permissions.tenant_owner;
+			},
+		}) //
+		.when('/tenant/invoices/new', {
+			templateUrl: 'views/amd-tenant/invoice-new.html',
+			controller: 'AmdTenantInvoiceController',
             /*
              * @ngInject
              */
-            protect: function ($rootScope) {
-                return !$rootScope.__account.permissions.tenant_owner;
-            },
-        }) //
-        .when('/tenant/invoices/:invoiceId', {
-            templateUrl: 'views/amd-tenant/invoice.html',
-            controller: 'AmdTenantInvoiceController',
-            helpId: 'invoiceDetails',
+			protect: function($rootScope) {
+				return !$rootScope.__account.permissions.tenant_owner;
+			},
+		}) //
+		.when('/tenant/invoices/:invoiceId', {
+			templateUrl: 'views/amd-tenant/invoice.html',
+			controller: 'AmdTenantInvoiceController',
+			helpId: 'invoiceDetails',
             /*
              * @ngInject
              */
-            protect: function ($rootScope) {
-                return !$rootScope.__account.permissions.tenant_owner;
-            },
-        }) //
-        .when('/receipts/:id', {
-            templateUrl: 'views/amd-tenant-receipt.html',
-            controller: 'AmdTenantReceiptCtrl',
-            protect: true,
-            helpId: 'receipt'
-        })
+			protect: function($rootScope) {
+				return !$rootScope.__account.permissions.tenant_owner;
+			},
+		}) //
+		.when('/receipts/:id', {
+			templateUrl: 'views/amd-tenant-receipt.html',
+			controller: 'AmdTenantReceiptCtrl',
+			protect: true,
+			helpId: 'receipt'
+		})
 
-        .when('/tenant/settings/security', {
-            templateUrl: 'views/amd-setting-security.html',
-            controller: 'AmdSettingsSecurityCtrl',
-            navigate: true,
-            groups: ['tenant'],
-            name: 'Security',
-            icon: 'font_download',
-            helpId: 'setting-captcha',
+		.when('/tenant/settings/security', {
+			templateUrl: 'views/amd-setting-security.html',
+			controller: 'AmdSettingsSecurityCtrl',
+			navigate: true,
+			groups: ['tenant'],
+			name: 'Security',
+			icon: 'font_download',
+			helpId: 'setting-captcha',
             /*
              * @ngInject
              */
-            protect: function ($rootScope) {
-                return !$rootScope.__account.permissions.tenant_owner;
-            },
-        }).when('/tenant/settings/local-setting', {
-            templateUrl: 'views/amd-local-setting.html',
-            controller: 'AmdLocalSettingsCtrl',
-            navigate: true,
-            groups: ['tenant'],
-            name: 'Local settings',
-            icon: 'settings_applications',
-            helpId: '',
+			protect: function($rootScope) {
+				return !$rootScope.__account.permissions.tenant_owner;
+			},
+		}).when('/tenant/settings/local-setting', {
+			templateUrl: 'views/amd-local-setting.html',
+			controller: 'AmdLocalSettingsCtrl',
+			navigate: true,
+			groups: ['tenant'],
+			name: 'Local settings',
+			icon: 'settings_applications',
+			helpId: '',
             /*
              * @ngInject
              */
-            protect: function ($rootScope) {
-                return !$rootScope.__account.permissions.tenant_owner;
-            },
-        });
+			protect: function($rootScope) {
+				return !$rootScope.__account.permissions.tenant_owner;
+			},
+		});
 });
