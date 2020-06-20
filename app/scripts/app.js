ab0c0de360cb4888f7d2b2ca34a4a8a0c98a9e9a
Mostafa Barmshory <mostafa.barmshory@gmail.com>
2020-06-20 08:10:38 +0430

    Account toolbar is added.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -22,170 +22,180 @@
  * SOFTWARE.
  */
 
-var APP_KEY = 'vwstudio';
 /***********************************************************************************
  * Configuration
  **********************************************************************************/
-mblowfish.config(function(
-	$mbApplicationProvider, $mbLayoutProvider, $mbToolbarProvider, $mbActionsProvider,
-	$mbSidenavProvider,
-	$mbAccountProvider,
-	$mbTranslateSanitizationProvider,
-	$mbStorageProvider, $locationProvider) {
+mblowfish
+	.addConstants({
+		APP_KEY: 'vwstudio',
+		AMD_ACCOUNT_TOOLBAR_COMPONENT: 'vw.account.toolbar.component',
+	})
+	.config(function(
+		$mbApplicationProvider, $mbLayoutProvider, $mbToolbarProvider, $mbActionsProvider,
+		$mbSidenavProvider,
+		$mbAccountProvider,
+		$mbTranslateSanitizationProvider,
+		$mbStorageProvider, $locationProvider) {
 
 
-	$mbAccountProvider
-		.addAuthenticationProvider('AmdUserAtuthenticationProvider');
+		$mbAccountProvider
+			.addAuthenticationProvider('AmdUserAtuthenticationProvider');
 
-	$mbTranslateSanitizationProvider
-		.useStrategy(['sanitize']);
+		$mbTranslateSanitizationProvider
+			.useStrategy(['sanitize']);
 
-	//
-	// Application ID
-	//
-	// Application ID is used to seperate applications from each other. for
-	// example you may have studo and dashboard application.
-	//
-	$mbApplicationProvider
-		.setKey(APP_KEY)
-		.setPreloadingEnabled(true)
-		.setAccountDetailRequired(true)
-		.setSettingsRequired(true)
-		.setLogingRequired(true)
-		.setLoginComponent({
-			templateUrl: 'views/account/login-default.html',
-			controller: 'MbAccountContainerCtrl',
-			controllerAs: 'ctrl'
-		})
-		.addAction('init', {
-			title: 'Loading Tenant Settings',
-			/* @ngInject */
-			action: function($mbTenant) {
-				return $mbTenant.reload();
-			}
-		});
+		//
+		// Application ID
+		//
+		// Application ID is used to seperate applications from each other. for
+		// example you may have studo and dashboard application.
+		//
+		$mbApplicationProvider
+			.setKey(APP_KEY)
+			.setPreloadingEnabled(true)
+			.setAccountDetailRequired(true)
+			.setSettingsRequired(true)
+			.setLogingRequired(true)
+			.setLoginComponent({
+				templateUrl: 'views/account/login-default.html',
+				controller: 'MbAccountContainerCtrl',
+				controllerAs: 'ctrl'
+			})
+			.addAction('init', {
+				title: 'Loading Tenant Settings',
+				/* @ngInject */
+				action: function($mbTenant) {
+					return $mbTenant.reload();
+				}
+			});
 
-	//
-	// Application storage prefix
-	//
-	//  All data will be stored in local storage with key. This will be
-	// added to all keys. So you can run several application which is 
-	// designed based on MB
-	$mbStorageProvider.setKeyPrefix(APP_KEY + '.');
+		//
+		// Application storage prefix
+		//
+		//  All data will be stored in local storage with key. This will be
+		// added to all keys. So you can run several application which is 
+		// designed based on MB
+		$mbStorageProvider.setKeyPrefix(APP_KEY + '.');
 
-	//
-	// HTML5 Addess style
-	//
-	// Enables HTML5 addresss style. SO the #! sign will be removed from
-	// the path.
-	$locationProvider.html5Mode(true);
+		//
+		// HTML5 Addess style
+		//
+		// Enables HTML5 addresss style. SO the #! sign will be removed from
+		// the path.
+		$locationProvider.html5Mode(true);
 
-	//
-	//  $mbLayout: manages layouts of the system. It is used as a basic layout
-	// system to manage views, editors and etc. You are free to add layouts dynamically
-	// at runtime.
-	//
-	// $mbLayoutProvider.setMode('auto');
-	$mbLayoutProvider
-		.setDefault('Demo Layout')
-		.addLayout('Demo Layout', {
-			settings: {
-				hasHeaders: true,
-				constrainDragToContainer: true,
-				reorderEnabled: true,
-				selectionEnabled: true,
-				popoutWholeStack: false,
-				blockedPopoutsThrowError: true,
-				closePopoutsOnUnload: true,
-				showPopoutIcon: false,
-				showMaximiseIcon: true,
-				showCloseIcon: true
-			},
-			dimensions: {
-				borderWidth: 5,
-				minItemHeight: 16,
-				minItemWidth: 50,
-				headerHeight: 20,
-				dragProxyWidth: 300,
-				dragProxyHeight: 200
-			},
-			content: [{
-				id: 'main',
-				type: 'row',
-				isClosable: false,
-				componentState: {},
+		//
+		//  $mbLayout: manages layouts of the system. It is used as a basic layout
+		// system to manage views, editors and etc. You are free to add layouts dynamically
+		// at runtime.
+		//
+		// $mbLayoutProvider.setMode('auto');
+		$mbLayoutProvider
+			.setDefault('Demo Layout')
+			.addLayout('Demo Layout', {
+				settings: {
+					hasHeaders: true,
+					constrainDragToContainer: true,
+					reorderEnabled: true,
+					selectionEnabled: true,
+					popoutWholeStack: false,
+					blockedPopoutsThrowError: true,
+					closePopoutsOnUnload: true,
+					showPopoutIcon: false,
+					showMaximiseIcon: true,
+					showCloseIcon: true
+				},
+				dimensions: {
+					borderWidth: 5,
+					minItemHeight: 16,
+					minItemWidth: 50,
+					headerHeight: 20,
+					dragProxyWidth: 300,
+					dragProxyHeight: 200
+				},
 				content: [{
-					type: 'column',
+					id: 'main',
+					type: 'row',
 					isClosable: false,
+					componentState: {},
 					content: [{
-						id: 'editors',
-						type: 'stack',
-						title: 'Editors',
+						type: 'column',
 						isClosable: false,
-						componentState: {},
 						content: [{
-							type: 'component',
-							componentName: 'component',
-							title: 'Welcome',
+							id: 'editors',
+							type: 'stack',
+							title: 'Editors',
 							isClosable: false,
-							isEditor: true,
-							url: '/mb/iframe/https://www.viraweb123.ir/wb/content/angular-material-dashboard-default-en',
-							componentState: {
+							componentState: {},
+							content: [{
+								type: 'component',
+								componentName: 'component',
+								title: 'Welcome',
+								isClosable: false,
 								isEditor: true,
 								url: '/mb/iframe/https://www.viraweb123.ir/wb/content/angular-material-dashboard-default-en',
-								params: {
-									url: 'https://www.viraweb123.ir/wb/content/angular-material-dashboard-default-en',
+								componentState: {
+									isEditor: true,
+									url: '/mb/iframe/https://www.viraweb123.ir/wb/content/angular-material-dashboard-default-en',
+									params: {
+										url: 'https://www.viraweb123.ir/wb/content/angular-material-dashboard-default-en',
+									}
 								}
-							}
+							}]
 						}]
 					}]
 				}]
-			}]
-		});
+			});
 
 
-	//
-	// $mbAction: manages all actions
-	//
-	$mbActionsProvider
-		.init({
-			items: {
-				'mb.app.navigator.toggle': {
-					title: 'Navigator',
-					description: 'Tooble Navigator Sidenav',
-					icon: 'menu',
-					/* @ngInject */
-					action: function($mbSidenav) {
-						$mbSidenav.getSidenav('/app/navigator').toggle();
+		//
+		// $mbAction: manages all actions
+		//
+		$mbActionsProvider
+			.init({
+				items: {
+					'mb.app.navigator.toggle': {
+						title: 'Navigator',
+						description: 'Tooble Navigator Sidenav',
+						icon: 'menu',
+						/* @ngInject */
+						action: function($mbSidenav) {
+							$mbSidenav.getSidenav('/app/navigator').toggle();
+						}
 					}
 				}
-			}
+			});
+
+		$mbSidenavProvider.addSidenav('/app/navigator', {
+			title: 'Navigator',
+			description: 'Navigate all path and routs of the pandel',
+			controller: 'MbNavigatorContainerCtrl',
+			controllerAs: 'ctrl',
+			templateUrl: 'views/mb-navigator.html',
+			locked: 'false',
+			position: 'start'
 		});
-
-	$mbSidenavProvider.addSidenav('/app/navigator', {
-		title: 'Navigator',
-		description: 'Navigate all path and routs of the pandel',
-		controller: 'MbNavigatorContainerCtrl',
-		controllerAs: 'ctrl',
-		templateUrl: 'views/mb-navigator.html',
-		locked: 'false',
-		position: 'start'
-	});
-	// $mbRouteProvider.otherwise('/dashboard');
+		// $mbRouteProvider.otherwise('/dashboard');
 
 
-	//
-	//  By initializing the main toolbar you can add list of action or component
-	// into the toolbar.
-	//
-	$mbToolbarProvider.init([{
-		url: '/dashboard',
-		items: ['mb.app.navigator.toggle']
-	}, {
-		url: '/cms',
-		items: []
-	}]);
-})
+		//
+		//  By initializing the main toolbar you can add list of action or component
+		// into the toolbar.
+		//
+		$mbToolbarProvider.init([{
+			url: '/dashboard',
+			items: ['mb.app.navigator.toggle']
+		}, {
+			url: '/cms',
+			items: []
+		}, {
+			url: '/user/account',
+			float: 'right',
+			items: [
+				AMD_ACCOUNT_TOOLBAR_COMPONENT
+			]
+		}]);
+	})
 
 
 	/***********************************************************************************
@@ -208,7 +218,23 @@ $(window).on('load', function() {
 		.loadModules(APP_KEY)
 		.then(function() {
 			try {
-				mblowfish.bootstrap(document.documentElement);
+				mblowfish.bootstrap(document.documentElement, [
+					//	load legecy angular modules
+					'seen-core',
+					'seen-user',
+					'seen-tenant',
+					'seen-supertenant',
+					'seen-cms',
+					'seen-monitor',
+					'seen-shop',
+					'seen-sdp',
+					'seen-seo',
+
+					'vcRecaptcha', //https://github.com/VividCortex/angular-recaptcha
+					'ngFileSaver',//
+					'mdSteppers',//
+					'mdColorPicker',
+				]);
 			} catch (error) {
 				console.log(error);
 			}
