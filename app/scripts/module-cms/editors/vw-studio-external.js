afaea6e8d8195f30c2890ae9b4c3ae6fe941200c
Mostafa Barmshory <mostafa.barmshory@gmail.com>
2020-08-31 07:14:32 +0430

    vw-studio is added as external and internal editor.

---
+++ app/scripts/module-cms/editors/vw-studio-external.js
@@ -0,0 +1,52 @@
+mblowfish.editor('/cms/vw-studio-editor-ext/:contentId', {
+	title: 'VW Studio External',
+	icon: 'text',
+	template: '<iframe></iframe>',
+	controllerAs: 'ctrl',
+	supportedMimetypes: [
+		'application/weburger+json'
+	],
+	controller: function($state, $editor, $cms, $httpParamSerializer, $mbLocal, $window) {
+		'ngInject';
+		//------------------------------------------------------------------
+		// Functions
+		//------------------------------------------------------------------\
+		function loadObject() {
+			$cms
+				.getContent($state.params.contentId, {
+					graphql: '{id,media_type,mime_type,metas{id,key,value}}'
+				})
+				.then(function(content) {
+					var metas = content.metas;
+					delete content.metas;
+					var language = $mbLocal.getLanguage();
+					_.forEach(metas, function(meta) {
+						if (meta.key === 'language') {
+							language = meta.value;
+						}
+					});
+					content = _.assign(content, {
+						language: language,
+						url: '/api/v2/cms/contents/' + content.id + '/content'
+					});
+					// XXX: maso, 2020: this will be replaced with common editor api
+					$window.open('/vw-studio/pages/' + content.id + '?' + $httpParamSerializer(content), '_blank');
+					$editor.close();
+				});
+		}
+
+		//------------------------------------------------------------------
+		// init
+		//------------------------------------------------------------------
+		loadObject();
+	},
+});
+
+
+
+
+
+
+
+
+
