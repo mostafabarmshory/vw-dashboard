e5a1d1b9daa50df1eed288895902204de093deb6
Mostafa Barmshory <mostafa.barmshory@gmail.com>
2020-12-16 01:28:08 +0330

    Hot key test

--- app/scripts/module-seo/views/sitemaps-view.js
+++
@@ -1,6 +0,0 @@
-mblowfish.addView('/seo/sitemap', {
-	template: '<iframe style="border: 0px;" height="100%" id="sitemap" src="/api/v2/seo/sitemap/sitemap.xml"></iframe>',
-	groups: ['seo'],
-	title: 'View Sitemap',
-	icon: 'public'
-});
